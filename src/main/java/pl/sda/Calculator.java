fdefbafb4103222fb8967c71be776a3572b111c3
Bartlomiej Janik <yanekk1990@gmail.com>
2020-08-22 14:01:09 +0200

    zad 12

--- src/main/java/pl/sda/Calculator.java
+++ src/main/java/pl/sda/Calculator.java
@@ -1,7 +1,12 @@
 package pl.sda;
 
+import lombok.Setter;
+
+@Setter
 public class Calculator {
-    public int square(int n) {
+    private int n;
+
+    public int square() {
         return n * n;
     }
 }
