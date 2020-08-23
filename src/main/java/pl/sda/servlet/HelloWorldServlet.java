717580cb53e3a7b86f716bcad7db2477659a0f30
Bartlomiej Janik <yanekk1990@gmail.com>
2020-08-23 12:59:48 +0200

    zad 24

--- src/main/java/pl/sda/servlet/HelloWorldServlet.java
+++ src/main/java/pl/sda/servlet/HelloWorldServlet.java
@@ -9,9 +9,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWordlServlet", urlPatterns = {"/", "/hello"}, initParams =
-        {@WebInitParam(name = "id", value = "123456"),
-                @WebInitParam(name = "localization", value = "Poland")}, loadOnStartup = 1)
+//@WebServlet(name = "HelloWordlServlet", urlPatterns = {"/", "/hello"}, initParams =
+//        {@WebInitParam(name = "id", value = "123456"),
+//                @WebInitParam(name = "localization", value = "Poland")}, loadOnStartup = 1)
 public class HelloWorldServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
